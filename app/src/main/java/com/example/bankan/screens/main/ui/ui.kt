00918b5e70ff67ae745d8a79acdc315a168ec40a
ariel <nikitosiks338@gmail.com>
2022-03-22 02:10:45 +0300

    DashOutline Composable extracted

--- app/src/main/java/com/example/bankan/screens/main/ui/ui.kt
+++ app/src/main/java/com/example/bankan/screens/main/ui/ui.kt
@@ -2,6 +2,8 @@ package com.example.bankan.screens.main.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
@@ -12,14 +14,15 @@ import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.StrokeJoin
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Density
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.bankan.common.ui.theme.BankanTheme
-import kotlin.math.abs
-import kotlin.math.sin
 
 
 @Preview
@@ -40,43 +43,79 @@ fun BoardListPreview() {
 fun BoardList(list: List<String>) {
     Column {
         EachAndBetween(data = list) {
-            BoardCard(it)
+            BoardCard(boardName = it)
+        }
+        Spacer(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(10.dp)
+        )
+    }
+}
+
+
+@Composable
+fun BoardCard(modifier: Modifier = Modifier, boardName: String) {
+    val vm = viewModel<AnimationViewModel>()
+    val time by vm.times.collectAsState()
+
+    fun Int.breakOn(pointOfBreak: Int): Int {
+        return if (this <= pointOfBreak) this else pointOfBreak * 2 - this
+    }
+
+    val intervals = floatArrayOf(
+        time.mod(101).plus(20).breakOn(70).toFloat(),
+        70.minus(time.mod(101).plus(20).breakOn(70)).toFloat(),
+    )
+    BankanTheme {
+        DashOutline(
+            modifier = modifier,
+            strokeColor = Color.Cyan,
+            intervals = intervals,
+            phase = -time.toFloat()
+        ) {
+            Text(modifier = Modifier.padding(10.dp), text = boardName)
         }
     }
 }
 
-
-
 @Composable
-fun BoardCard(boardName: String) {
-    val vm = viewModel<AnimationViewModel>()
-    val time by vm.times.collectAsState()
-    val space = abs(sin(time.toFloat() / 66f)) * 20f
-
-    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(26f, space), 0f)
+fun DashOutline(
+    modifier: Modifier = Modifier,
+    cornersSize: CornerSize = MaterialTheme.shapes.medium.topStart,
+    strokeColor: Color = MaterialTheme.colors.secondary,
+    strokeWidth: Float = 6f,
+    intervals: FloatArray = floatArrayOf(strokeWidth * 6f, strokeWidth * 3f),
+    phase: Float = 0f,
+    content: @Composable () -> Unit
+) {
+    val pathEffect = PathEffect.dashPathEffect(intervals, phase)
     val stroke = Stroke(
-        width = 3f,
-        pathEffect = pathEffect
+        width = strokeWidth,
+        pathEffect = pathEffect,
+        cap = StrokeCap.Round,
+        join = StrokeJoin.Round
     )
-    BankanTheme {
-            Box(
-                modifier = Modifier
-                    .background(Color.Transparent)
-                    .drawBehind {
-                        drawRoundRect(
-                            color = Color.Cyan,
-                            style = stroke,
-                            cornerRadius = CornerRadius(10f),
-                            topLeft = Offset(2f, 2f),
-                            size = size.copy(width = size.width - 4f, height = size.height - 4f)
-                        )
-                    },
-            ) {
-                Text(modifier = Modifier.padding(10.dp), text = boardName)
-            }
+    Box(
+        modifier = modifier
+            .background(Color.Transparent)
+            .drawBehind {
+                val density = Density(density, fontScale)
+                drawRoundRect(
+                    color = strokeColor,
+                    style = stroke,
+                    cornerRadius = CornerRadius(cornersSize.toPx(size, density)),
+                    topLeft = Offset(stroke.width / 2f, stroke.width / 2f),
+                    size = size.copy(
+                        width = size.width - stroke.width,
+                        height = size.height - stroke.width
+                    )
+                )
+            },
+    ) {
+        content()
     }
 }
-
 
 @Composable
 fun <T> EachAndBetween(
