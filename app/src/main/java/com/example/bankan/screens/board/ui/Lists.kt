21fb48409dfba2665c76a08e9479369bb5105c08
ariel <nikitosiks338@gmail.com>
2022-04-13 00:29:55 +0300

    list visual improvements

--- app/src/main/java/com/example/bankan/screens/board/ui/Lists.kt
+++ app/src/main/java/com/example/bankan/screens/board/ui/Lists.kt
@@ -1,22 +1,21 @@
 package com.example.bankan.screens.board.ui
 
+import androidx.compose.animation.core.TargetBasedAnimation
+import androidx.compose.animation.core.VectorConverter
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.dp
+import com.example.bankan.common.ui.EachAndBetween
 import com.example.bankan.common.ui.components.CreateNewButton
 import com.example.bankan.common.ui.components.SwipeableElement
 import com.example.bankan.common.ui.theme.BankanTheme
@@ -31,37 +30,52 @@ import org.koin.androidx.compose.viewModel
 fun List1(data: ListData) {
     val vm: BoardScreenViewModel by viewModel()
     BankanTheme {
-        LazyColumn(
+        Column(
             modifier = Modifier
                 .background(Color.DarkGray, RoundedCornerShape(20.dp))
-                .width(280.dp)
-                .heightIn(max = LocalConfiguration.current.screenHeightDp.dp),
+                .width(280.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            item {
-                Text(
-                    text = data.info.name,
-                    style = MaterialTheme.typography.h6,
-                    modifier = Modifier
-                        .background(Color.Gray, RoundedCornerShape(20.dp))
-                        .fillMaxWidth()
-                )
-            }
-            items(data.content) { card ->
-                Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = data.info.name,
+                style = MaterialTheme.typography.h6,
+                modifier = Modifier
+                    .background(Color.Gray, RoundedCornerShape(20.dp))
+                    .fillMaxWidth()
+            )
+            EachAndBetween(data = data.content) { card ->
                 SwipeableElement(onSwipe = { vm.deleteCard(card.localId) }) {
-                    val modifier = it.animateItemPlacement(tween(500))
+
+                    val anim = remember {
+                        TargetBasedAnimation(
+                            animationSpec = tween(500),
+                            typeConverter = Float.VectorConverter,
+                            initialValue = 0f,
+                            targetValue = 1f
+                        )
+                    }
+                    var playTime by remember { mutableStateOf(0L) }
+                    var factor by remember { mutableStateOf(0f) }
+
+                    LaunchedEffect(anim) {
+                        val startTime = withFrameNanos { it }
+                        do {
+                            playTime = withFrameNanos { it } - startTime
+                            factor = anim.getValueFromNanos(playTime)
+                        } while (factor < 1f)
+                    }
+
+                    val modifier = it.scale(scaleY = factor, scaleX = 1f)
+
                     if (card.name.isNotEmpty())
                         Card(modifier = modifier, name = card.name, description = card.description)
                     else
                         NameLessCard(modifier = modifier, card.description)
                 }
             }
-            item {
-                Spacer(modifier = Modifier.height(10.dp))
-                AddNewCard(listInfo = data.info)
-                Spacer(modifier = Modifier.height(10.dp))
-            }
+            Spacer(modifier = Modifier.height(10.dp))
+            AddNewCard(listInfo = data.info)
+            Spacer(modifier = Modifier.height(10.dp))
         }
     }
 }
