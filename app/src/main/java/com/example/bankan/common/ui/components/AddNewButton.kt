1d33d29e9a466e9aa33d9a793d2d4b90397efb23
ariel <nikitosiks338@gmail.com>
2022-04-12 14:12:53 +0300

    focus on AddNewButton.kt fixed, scroll to on focus added

--- app/src/main/java/com/example/bankan/common/ui/components/AddNewButton.kt
+++ app/src/main/java/com/example/bankan/common/ui/components/AddNewButton.kt
@@ -2,6 +2,9 @@ package com.example.bankan.common.ui.components
 
 import androidx.compose.animation.*
 import androidx.compose.animation.core.tween
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
@@ -10,15 +13,20 @@ import androidx.compose.material.TextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.layout.onPlaced
+import androidx.compose.ui.focus.onFocusEvent
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalComposeUiApi::class, ExperimentalAnimationApi::class)
 @Composable
@@ -30,7 +38,6 @@ fun CreateNewButton(
     onNameChanged: (String) -> Unit,
     onSubmit: () -> Unit
 ) {
-    val tfFr = remember { FocusRequester() }
     DashOutline(modifier = modifier) {
         AnimatedContent(
             targetState = isEntering,
@@ -39,8 +46,8 @@ fun CreateNewButton(
         { targetState ->
             if (targetState) TextField(
                 modifier = Modifier
-                    .focusRequester(tfFr)
-                    .onPlaced { tfFr.requestFocus() },
+                    .scrollToOnFocus()
+                    .focusOnEntry(),
                 value = name,
                 onValueChange = { onNameChanged(it) },
                 keyboardOptions = KeyboardOptions(
@@ -54,4 +61,30 @@ fun CreateNewButton(
             }
         }
     }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+fun Modifier.scrollToOnFocus() = composed {
+    val scope = rememberCoroutineScope()
+
+    val requester = remember { BringIntoViewRequester() }
+
+    bringIntoViewRequester(requester).onFocusEvent {
+        if (it.isFocused) {
+            scope.launch {
+                delay(250)
+                requester.bringIntoView()
+            }
+        }
+    }
+}
+
+fun Modifier.focusOnEntry() = composed {
+    val focusRequester = remember { FocusRequester() }
+
+    LaunchedEffect(true) {
+        focusRequester.requestFocus()
+    }
+
+    focusRequester(focusRequester = focusRequester)
 }
