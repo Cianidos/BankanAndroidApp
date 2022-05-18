277ea6944d1c8e8e0624cdebc5d237998c36e2ce
ariel <nikitosiks338@gmail.com>
2022-05-19 00:26:41 +0300

    material theme colors overridden

--- app/src/main/java/com/example/bankan/common/ui/theme/Theme.kt
+++ app/src/main/java/com/example/bankan/common/ui/theme/Theme.kt
@@ -1,10 +1,10 @@
 package com.example.bankan.common.ui.theme
 
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = Purple200,
@@ -13,27 +13,25 @@ private val DarkColorPalette = darkColors(
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
-
-    /* Other default colors to override
-    background = Color.White,
-    surface = Color.White,
+    primary = Color(0xFF494e50),
+    primaryVariant = Color(0xFFc5c5c5),
+    secondary = Color(0xFF26D9D9),
+    background = Color(0xFFdbdbdb),
+    surface = Color(0xFFf6f5f5),
     onPrimary = Color.White,
     onSecondary = Color.Black,
     onBackground = Color.Black,
     onSurface = Color.Black,
-    */
 )
 
 @Composable
-fun BankanTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
-    val colors = if (darkTheme) {
-        DarkColorPalette
-    } else {
-        LightColorPalette
-    }
+fun BankanTheme(/*darkTheme: Boolean = isSystemInDarkTheme(),*/ content: @Composable () -> Unit) {
+//    val colors = if (darkTheme) {
+//        DarkColorPalette
+//    } else {
+//        LightColorPalette
+//    }
+    val colors = LightColorPalette
 
     MaterialTheme(
         colors = colors,
